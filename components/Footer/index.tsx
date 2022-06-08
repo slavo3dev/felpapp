a041594f35eaf6e3d65d89dce1dbdd332d2544ae
nikolaklenje <nikolaelite88@gmail.com>
2022-06-08 13:20:21 -0400

    Modified Header and Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 export const Footer = () =>
 (
     <footer>
+        <div className="text-center bg-gray-800 h-16 py-5 my-10 rounded-lg">
           Powered by{' '} <a href="https://www.slavo3.com/" target="_blank">FelpApp & Prototype.NEXT</a>
+        </div>
       </footer>
 )
